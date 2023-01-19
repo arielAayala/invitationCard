624253719df0c8637a97f5332cc3d2d3f4c775f0
arielAayala <ariel170802@gmail.com>
2023-01-19 15:32:44 -0300

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import mariposa from "../assets/static/mariposa.png"
+
+export default function Header() {
+    return(
+    <div className="row d-flex justify-content-center align-items-center">
+        <div className="text-center">
+            <div className="box-date-name">
+                <span className="fecha">
+                    17-08-2023
+                </span>
+                <h1>Ariel</h1>
+                <h2>Mis 15 años</h2>
+            </div>
+        </div>
+    </div>
+        
+    )
+};
